ce4f3784f09422551f3cc750997bdf41200b23a4
William <william.lonnberg@gmail.com>
2024-11-07 20:29:40 +0200

    Added apache expect eof

--- lamp-setup.sh
+++ lamp-setup.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 
 # Define a few color codes
-BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 
 # Install expect
-echo -e "${BLUE}Installing Expect...${$}"
+echo -e "${PURPLE}Installing Expect...${NC}"
 sudo apt install expect
 
+
 # Install Apache
-echo -e "${BLUE}Installing Apache...${NC}"
-sudo apt install apache2
+expect <<EOF
+echo -e "${PURPLE}Installing Apache...${NC}"
+spawn sudo apt install apache2
+expect "Continue"
+send "y\r"
+expect eof
+EOF
 
 # Append "ServerName 127.0.0.1" to Apache's configuration file
-echo -e "${BLUE}Adding localhost server name to configuration file...${NC}"
+echo -e "${PURPLE}Adding localhost server name to configuration file...${NC}"
 if ! grep -q "ServerName 127.0.0.1" /etc/apache2/apache2.conf; then
     echo "ServerName 127.0.0.1" | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 fi
 
 # Install MySQL server
-echo -e "${BLUE}Installing MySQL server...${NC}"
+echo -e "${PURPLE}Installing MySQL server...${NC}"
 sudo apt install mysql-server
 
 # Run the secure installation script for MySQL
-echo -e "${BLUE}Running MySQL secure installation...${NC}"
+echo -e "${PURPLE}Running MySQL secure installation...${NC}"
 
 expect <<EOF
 spawn sudo mysql_secure_installation
 
-# Handle the sudo password prompt
+# Handle the sudo password prompt 
 expect "password for *:"
 send "changeme\r"
 
 # Proceed with MySQL secure installation and set default values
 expect "VALIDATE PASSWORD COMPONENT"
 send "y\r"
-expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
-send "1\r"
 expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
 expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
@@ -48,13 +52,13 @@ send "y\r"
 expect eof
 EOF
 
-echo -e "${BLUE}MySQL secure installation ready...${NC}"
+echo -e "${PURPLE}MySQL secure installation ready...${NC}"
 
-echo -e "${BLUE}Installing PHP...${NC}"
+echo -e "${PURPLE}Installing PHP...${NC}"
 sudo apt install -y php libapache2-mod-php php-mysql
 
 # Uninstall redundant files
-echo -e "${BLUE}Cleaning up unnecessary packages...${NC}"
+echo -e "${PURPLE}Cleaning up unnecessary packages...${NC}"
 sudo apt autoremove
 
 # Print Apache, MySQL and PHP versions
@@ -63,10 +67,10 @@ mysql --version
 php -v
 
 # Reload system configuration and restart Apache
-echo -e "${BLUE}Reloading daemon...${NC}"
+echo -e "${PURPLE}Reloading daemon...${NC}"
 sudo systemctl daemon-reload
-echo -e "${BLUE}Restaring Apache...${NC}"
+echo -e "${PURPLE}Restaring Apache...${NC}"
 sudo systemctl restart apache2.service
 
-echo -e "${BLUE}LAMP stack installation complete!${NC}"
+echo -e "${PURPLE}LAMP stack installation complete!${NC}"
 
